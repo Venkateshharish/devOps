330ae306101066e7c89199d29dddeb341b093f45
Venkateshharish <nvhari05@gmail.com>
2022-09-02 16:57:00 +0530

    Docker Run

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ require("./app/routes/turorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running in port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
